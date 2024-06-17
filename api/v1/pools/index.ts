d9177b784d0760b20f330f77e58fe0a613dc382e
Samus <26967164+SamusElderg@users.noreply.github.com>
2024-06-17 14:27:44 +0800

    bump up cache

--- api/v1/pools/index.ts
+++ api/v1/pools/index.ts
@@ -117,7 +117,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
         );
       }
       topPools = response.data.data.pools;
-      await kv.set("topPools", topPools, { ex: 60 }); // TODO:set to 21600
+      await kv.set("topPools", topPools, { ex: 21600 }); // 6 hours
     } catch (error) {
       res.status(500).json({
         error: {
@@ -191,7 +191,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     return;
   }
 
-  // Set the Cache-Control header to cache the response for 15 minutes for clients & CDNs
-  res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate"); // TODO: set to 900
+  // Set the Cache-Control header to cache the response for 60 minutes for clients & CDNs
+  res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
   res.status(200).json(reducedPoolsData);
 };
